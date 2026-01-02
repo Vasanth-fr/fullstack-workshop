ef22486850cbac4c419817777678df72e1927123
unknown <hp@Velocityy.M>
2026-01-02 12:19:14 +0530

    Adding MY SQL challenges

---
+++ 09-mysql/challenge-01/department-stats.sql
@@ -0,0 +1,8 @@
+SELECT
+    department,
+    count(*) AS employee_count,
+    ROUND(AVG(salary), 2) AS avg_salary,
+    MAX(salary) AS max_salary
+FROM employees
+GROUP BY department
+HAVING COUNT(*) > 2;
